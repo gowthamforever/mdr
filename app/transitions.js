4f667f22006d0e4fbc2e33e0572694a33c976f9e
Anand Sakthivel <anandts88@gmail.com>
2016-01-06 19:15:11 -0500

    Transition

--- app/transitions.js
+++ app/transitions.js
@@ -2,6 +2,13 @@ export default function() {
   const duration = 350;
 
   this.transition(
+    this.fromRoute('account.signin'),
+    this.toRoute('account.signup'),
+    this.use('crossFade', { duration }),
+    this.reverse('crossFade', { duration })
+  );
+
+  this.transition(
     this.use('toRight', { duration })
   );
 }
