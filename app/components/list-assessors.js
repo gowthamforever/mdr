984551c27daa12d120c79b3aa3c19ff0adc44b27
Anand Sakthivel <anandts88@gmail.com>
2016-01-29 23:41:33 -0500

    Filters

--- app/components/list-assessors.js
+++ app/components/list-assessors.js
@@ -5,65 +5,37 @@ const {
   isEmpty,
   get,
   set,
-  on,
-  computed,
-  inject
+  computed
 } = Ember;
 
 const {
   oneWay
 } = computed;
 
-const {
-  service
-} = inject;
-
 export default Component.extend({
-  dialog: service(),
-
   tagName: 'section',
   selected: null,
   filtered: oneWay('model.assessors'),
-
-  initFiltered: on('didInitAttrs', function() {
-    const selected  = this.get('selected');
-    if (selected) {
-      set(this, 'filtered', Ember.A([selected]));
-    }
-  }),
 
   actions: {
     filter() {
       const model     = this.get('model');
       const firstName = model.get('firstName');
       const lastName  = model.get('lastName');
-      const assessors = model.get('assessors');
+      const assessors   = model.get('assessors');
 
       if (!isEmpty(assessors)) {
-        set(this, 'filtered', assessors.filter((assessor) =>
-          get(assessor, 'first_name').indexOf(firstName) !== -1 ||
-          get(assessor, 'last_name').indexOf(lastName) !== -1));
+        set(this, 'filtered', assessors.filter((assessor) => {
+          let result = true;
+          if (firstName) {
+            result = get(assessor, 'first_name').toLowerCase().indexOf(firstName.toLowerCase()) !== -1;
+          }
+          if (lastName) {
+            result = get(assessor, 'last_name').toLowerCase().indexOf(lastName.toLowerCase()) !== -1;
+          }
+          return result;
+        }));
       }
-    },
-
-    select(assessor) {
-      const model     = this.get('model');
-      const assessors = model.get('assessors');
-      const selected  = this.get('selected');
-      if (!selected) {
-        this.set('selected', assessor);
-        set(this, 'filtered', Ember.A([assessor]));
-      } else {
-        this.set('selected', null);
-        set(this, 'filtered', Ember.A(assessors));
-      }
-    },
-
-    assessor(assessor) {
-      this.get('dialog').showDialog({
-        name: 'modal-assessor',
-        model: assessor
-      });
     }
   }
 });
--- app/components/list-doctors.js
+++ app/components/list-doctors.js
@@ -5,7 +5,6 @@ const {
   get,
   set,
   isEmpty,
-  on,
   computed
 } = Ember;
 
@@ -18,37 +17,32 @@ export default Component.extend({
   selected: null,
   filtered: oneWay('model.doctors'),
 
-  initFiltered: on('didInitAttrs', function() {
-    const selected = this.get('selected');
-    if (selected) {
-      set(this, 'filtered', Ember.A([selected]));
-    }
-  }),
-
   actions: {
     filter() {
       const model     = this.get('model');
       const firstName = model.get('firstName');
       const lastName  = model.get('lastName');
+      const speciality = model.get('speciality');
       const doctors   = model.get('doctors');
 
       if (!isEmpty(doctors)) {
-        set(this, 'filtered', doctors.filter((doctor) =>
-          get(doctor, 'first_name').indexOf(firstName) !== -1 ||
-          get(doctor, 'last_name').indexOf(lastName) !== -1));
-      }
-    },
+        set(this, 'filtered', doctors.filter((doctor) => {
+          let result = true;
 
-    select(doctor) {
-      const model     = this.get('model');
-      const doctors   = model.get('doctors');
-      const selected = this.get('selected');
-      if (!selected) {
-        this.set('selected', doctor);
-        set(this, 'filtered', Ember.A([doctor]));
-      } else {
-        this.set('selected', null);
-        set(this, 'filtered', Ember.A(doctors));
+          if (speciality) {
+            result = get(doctor, 'primary_speciality') === speciality.id;
+          }
+
+          if (firstName) {
+            result = get(doctor, 'first_name').toLowerCase().indexOf(firstName.toLowerCase()) !== -1;
+          }
+
+          if (lastName) {
+            result = get(doctor, 'last_name').toLowerCase().indexOf(lastName.toLowerCase()) !== -1;
+          }
+
+          return result;
+        }));
       }
     }
   }
