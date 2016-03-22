805faae9c7941213e24950ce0095932cd6eaab8c
Baskar <gowthamforever.r@gmail.com>
2016-03-22 08:46:42 +0530

    Form five - form validations added

--- app/components/form-five.js
+++ app/components/form-five.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Form from 'mdr/models/form';
 import Api from 'mdr/mixins/api';
 import { retainNumbers } from 'mdr/utility/utils';
+import EmberValidator from 'ember-validator';
 
 const {
   Component,
@@ -13,7 +14,7 @@ const {
   service
 } = inject;
 
-export default Component.extend(Api, {
+export default Component.extend(Api, EmberValidator, {
   appointments: service(),
 
   props: [
@@ -45,38 +46,101 @@ export default Component.extend(Api, {
     this.set_form(this.get('form_model'), Form.create());
   }),
 
+  validations(model) {
+    return {
+      six_notywaip12m: {
+        required: 'This field is required'
+      },
+      six_date_of_last_arrest: {
+        required: 'This field is required'
+      },
+      six_reason_for_last_arrest: {
+        required: 'This field is required'
+      },
+      six_wladar: {
+        required: 'This field is required'
+      },
+      six_toduptla: {
+        required: 'This field is required'
+      },
+      six_ehawuda: {
+        required: 'This field is required'
+      },
+      six_ayciicjs: {
+        required: 'This field is required'
+      },
+      six_ayculs: {
+        required: 'This field is required'
+      },
+      six_wyopaa: {
+        required: 'This field is required'
+      },
+      six_pon: {
+        required: 'This field is required'
+      },
+      six_popn: {
+        required: 'This field is required'
+      },
+      six_toduptla2: {
+        required: 'This field is required'
+      },
+      six_wyottaacop: {
+        required: 'This field is required'
+      },
+      six_wycott: {
+        required: 'This field is required'
+      },
+      six_isbj: {
+        required: 'This field is required'
+      },
+      six_isbc: {
+        required: 'This field is required'
+      },
+      six_olhn: {
+        required: 'This field is required'
+      }
+    };
+  },
+
   actions: {
     next() {
       const self        = this;
       const appointment = self.get('model');
       const page        = this.get('page');
-      const form        = this.get('form');
+      let form          = this.get('form');
+      const validations = this.validations(form);
       let data;
+
+      form.set('validationResult', null);
 
       if (appointment.get('completed')) {
         if (page) {
           page(6);
         }
       } else {
-        data = _.pick(form, self.get('props'));
+        self.validateMap({ form, validations }).then(() => {
+          data = _.pick(form, self.get('props'));
 
-        data.six_date_of_last_arrest = moment(form.get('six_date_of_last_arrest_formatted'), 'MMM DD YYYY').format('YYYY-MM-DD');
-        data.six_popn = retainNumbers(form.get('six_popn'));
+          data.six_date_of_last_arrest = moment(form.get('six_date_of_last_arrest_formatted'), 'MMM DD YYYY').format('YYYY-MM-DD');
+          data.six_popn = retainNumbers(form.get('six_popn'));
 
-        self.ajax({
-          id: 'assessmentformpost',
-          path: {
-            id: appointment.get('id'),
-            pageNo: 5
-          },
-          data
-        }).then(() => {
-          self.set('appointments.cache', false);
-          self.set_form(form, self.get('form_model'));
-          if (page) {
-            page(6);
-          }
-        }).catch(Ember.K);
+          self.ajax({
+            id: 'assessmentformpost',
+            path: {
+              id: appointment.get('id'),
+              pageNo: 5
+            },
+            data
+          }).then(() => {
+            self.set('appointments.cache', false);
+            self.set_form(form, self.get('form_model'));
+            if (page) {
+              page(6);
+            }
+          }).catch(Ember.K);
+        }).catch((validationResult) => {
+          form.set('validationResult', validationResult);
+        });
       }
     },
 
